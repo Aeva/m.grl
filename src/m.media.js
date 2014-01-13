23930cbdf302e9574865f0593f124bad776692ef
Aeva Palecek <aeva.ntsc@gmail.com>
2014-01-12 18:47:52 -0600

    Fixed m.media bug where failed files were still getting the pass status in their callbacks.

--- src/m.media.js
+++ src/m.media.js
@@ -149,17 +149,24 @@ please.media.guess_type = function (file_name) {
 please.media.__xhr_helper = function (req_type, url, media_callback, user_callback) {
     var req = new XMLHttpRequest();
     please.media._push(req);
+    var handled = false;
     req.onload = function () {
-        please.media._pop(req);
-        media_callback(req);
-        if (typeof(user_callback) === "function") {
-            user_callback("pass", url);
+        if (!handled) {
+            handled = true;
+            please.media._pop(req);
+            media_callback(req);
+            if (typeof(user_callback) === "function") {
+                user_callback("pass", url);
+            }
         }
     };
     req.onerror = function () {
-        please.media._pop(req);
-        if (typeof(user_callback) === "function") {
-            user_callback("pass", url);
+        if (!handled) {
+            handled = true;
+            please.media._pop(req);
+            if (typeof(user_callback) === "function") {
+                user_callback("fail", url);
+            }
         }
     };
     req.open('GET', url, true);
