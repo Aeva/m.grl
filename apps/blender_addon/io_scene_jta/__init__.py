de2cda2fce1aea2bf6097742b3a5f4157701078b
Aeva Palecek <aeva@lunarfracking.com>
2014-10-16 18:16:53 -0500

    Implemented Blender addon scaffolding.

---
+++ apps/blender_addon/io_scene_jta/__init__.py
@@ -0,0 +1,121 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 2
+#  of the License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+
+
+bl_info = {
+    "name": "Export Mondaux Graphics and Recreation Library (.jta)",
+    "author": "Aeva Palecek",
+    "version": (0, 0, 0),
+    "blender": (2, 72, 0),
+    "location": "File > Import-Export",
+    "description": "Exports models and animation to M.GRL's json-based model format (.jta).",
+    "warning": "Incomplete support, work in progress.",
+    "wiki_url": "",
+    "category": "Import-Export"
+}
+
+if "bpy" in locals():
+    import imp
+    if "export_jta" in locals():
+        imp.reload(export_jta)
+
+
+import bpy
+from bpy.props import (BoolProperty,
+                       FloatProperty,
+                       StringProperty,
+                       EnumProperty,
+                       )
+from bpy_extras.io_utils import (ImportHelper,
+                                 ExportHelper,
+                                 path_reference_mode,
+                                 axis_conversion,
+                                 )
+
+
+class ExportJTA(bpy.types.Operator, ExportHelper):
+    """Save a M.GRL JTA File"""
+
+    bl_idname = "export_scene.jta"
+    bl_label = "Export JTA"
+    bl_options = {"PRESET"}
+
+    filename_ext = ".jta"
+    filter_glob = StringProperty(
+        default="*.jta",
+        options={"HIDDEN"},
+    )
+
+    # context group
+    use_selection = BoolProperty(
+        name="Selection Only",
+        description="Export selected objects only",
+        default=False,
+    )
+
+    # object group
+    use_mesh_modifiers = BoolProperty(
+        name="Apply Modifiers",
+        description="Apply modifiers (preview resolution)",
+        default=True,
+    )
+
+    # extra data group
+    use_smooth = BoolProperty(
+        name="Smooth Normals",
+        description="Hint that per-vertex normals should be averaged from adjacent face normals.",
+        default=False,
+    )
+
+    global_scale = FloatProperty(
+        name="Scale",
+        min=0.001, max=1000.0,
+        default=1.0,
+    )
+
+    path_mode = path_reference_mode
+
+    check_extension = True
+
+
+    def execute(self, context):
+        from . import export_jta
+        
+        keywords = self.as_keywords()
+        return export_jta.save(self, context, **keywords)
+
+
+
+
+def menu_func_export(self, context):
+    self.layout.operator(ExportJTA.bl_idname, text="M.GRL (.jta)")
+
+
+def register():
+    bpy.utils.register_module(__name__)
+    bpy.types.INFO_MT_file_export.append(menu_func_export)
+
+
+def unregister():
+    bpy.utils.unregister_module(__name__)
+    bpy.types.INFO_MT_file_export.remove(menu_func_export)
+
+
+if __name__ == "__main__":
+    register()
