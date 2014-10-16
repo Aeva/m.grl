792fcee216e0c7766cc747ddd1ebb3ed5c51d7c7
Aeva Palecek <aeva@lunarfracking.com>
2014-10-16 18:25:04 -0500

    cargo code

--- apps/blender_addon/io_scene_jta/export_jta.py
+++ apps/blender_addon/io_scene_jta/export_jta.py
@@ -23,13 +23,35 @@ import mathutils
 import bpy_extras.io_utils
 
 
+def mesh_triangulate(me):
+    import bmesh
+    bm = bmesh.new()
+    bm.from_mesh(me)
+    bmesh.ops.triangulate(bm, faces=bm.faces)
+    bm.to_mesh(me)
+    bm.free()
+
+
 def save(operator, context, filepath="",
          use_selection=False,
          use_mesh_modifiers=True,
          use_smooth=False,
          global_scale=None,
          path_mode='AUTO'):
+    
+    scene = context.scene
 
-    print("exporter called")
+    # Exit edit mode before exporting, so current object states are exported properly.
+    if bpy.ops.object.mode_set.poll():
+        bpy.ops.object.mode_set(mode='OBJECT')
+
+    if use_selection:
+        objects = context.selected_objects
+    else:
+        objects = scene.objects
+
+
+    print("FIXME: actually do something")
+
 
     return {"FINISHED"}
