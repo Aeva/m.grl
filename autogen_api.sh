454c95075768b6a4d6acd69da046558d873ee798
Aeva Palecek <aeva@lunarfracking.com>
2014-11-28 01:13:40 -0600

    updated shellscript for new autodoc command

--- autogen_api.sh
+++ autogen_api.sh
@@ -1,11 +1,1 @@
-export NODE_MODULES="$HOME/node_modules"
-export JSDOC="$NODE_MODULES/jsdoc/jsdoc.js"
-export THEME="$NODE_MODULES/jsdoc-rst-template/template"
-export DEST="docs/source/autogen"
-
-echo "Regenerating automatic api documentation..."
-rm -rf $DEST
-sh build.sh
-node $JSDOC mgrl.js --template $THEME --destination $DEST
-#node $JSDOC test.js --template $THEME --destination $DEST
-echo "Done!"
+js_autodoc src/ docs/source/api_reference/
