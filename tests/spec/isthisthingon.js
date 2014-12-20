d6ba7fbe8f38790cfb458d6032296f1b5e9ad0b3
Aeva Palecek <aeva@lunarfracking.com>
2014-12-20 12:06:48 -0600

    #52 added jasmine unit testing framework

---
+++ tests/spec/isthisthingon.js
@@ -0,0 +1,11 @@
+describe("Hello World", function () {
+
+    it("should run this unit test", function () {
+        expect(1).toBeTruthy();
+    });
+
+    it("should blow up on this unit test", function () {
+        expect(1).toBeFalsy();
+    });
+
+});
