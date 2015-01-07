78275a9c744be8a7cb7d22c15d69f654e6886022
Aeva Palecek <aeva@lunarfracking.com>
2015-01-06 22:13:58 -0600

    #50 building out a new index page

---
+++ site_media/navigation.js
@@ -0,0 +1,30 @@
+
+
+// connect a function here to respond to hash_change events
+if (window.change_focus === undefined) {
+    window.change_focus = null;
+}
+
+
+// a closure to create scope and connect some events
+(function () {
+    var last_hash = null;
+    
+    window.addEventListener("hashchange", function () {
+        var new_hash = document.location.hash;
+        if (last_hash && new_hash !== last_hash) {
+            if (change_focus) {
+                change_focus(last_hash, new_hash);
+            }
+            console.info(" - location hash changed to: " + new_hash);
+            last_hash = new_hash;
+        }
+    });
+    
+    window.addEventListener("load", function () {
+        if (document.location.hash === "") {
+            document.location.hash = "#about";
+        }
+        last_hash = document.location.hash;
+    });
+})();
---
+++ site_media/test_scene.js
@@ -0,0 +1,9 @@
+
+
+window.addEventListener("load", function () {
+});
+
+
+window.change_focus = function(last_focus, next_focus) {
+    console.info(" + changed focus to: " + next_focus + ", from: " + last_focus);
+};
