71c37e684e8049f9c47480d3818b85e750f28ecb
Aeva Palecek <aeva@lunarfracking.com>
2015-03-17 21:32:33 -0500

    Cleaned up old code to be more structurally consistent with the rest of mgrl.

--- src/m.time.js
+++ src/m.time.js
@@ -12,6 +12,14 @@
  */
 
 
+// Stores events for the scheduler.
+please.time = {
+    "__pending" : [],
+    "__times" : [],
+    "__last_frame" : performance.now(),
+};
+
+
 // [+] please.postpone(callback)
 //
 // Shorthand for setTimeout(callback, 0).  This method is used to
@@ -27,97 +35,80 @@ please.postpone = function (callback) {
 };
 
 
-please.time = (function () {
-    var batch = {
-        "__pending" : [],
-        "__times" : [],
-        "now" : performance.now(),
+// [+] please.time.schedule(callback, when)
+//
+// This function works like setTimeout, but syncs the callbacks up
+// only to the next available animation frame.  This means that if
+// the page is not currently visible (eg, another tab is active),
+// then the callback will not be called until the page is visible
+// again, etc.
+//
+// - **callback** A function to be called on an animation frame.
+//
+// - **when** Delay in milliseconds for the soonest time which 
+//   callback may be called.
+// 
+please.time.schedule = function (callback, when) {
+    when = please.time.__last_frame + when;
+    var i = please.time.__pending.indexOf(callback);
+    if (i > -1) {
+        please.time.__times[i] = when;
+    }
+    else {
+        please.time.__pending.push(callback);
+        please.time.__times.push(when);
 
-        "schedule" : function (callback, when) {},
-        "remove" : function (callback) {},
-    };
-    var dirty = false;
-    var pipe_id = "m.ani.js/batch";
-
-
-    // [+] please.time.schedule(callback, when)
-    //
-    // This function works like setTimeout, but syncs the callbacks up
-    // only to the next available animation frame.  This means that if
-    // the page is not currently visible (eg, another tab is active),
-    // then the callback will not be called until the page is visible
-    // again, etc.
-    //
-    // - **callback** A function to be called on an animation frame.
-    //
-    // - **when** Delay in milliseconds for the soonest time which 
-    //   callback may be called.
-    // 
-    batch.schedule = function (callback, when) {
-        when = batch.now + when;
-        var i = batch.__pending.indexOf(callback);
-        if (i > -1) {
-            batch.__times[i] = when;
+        // register a pipeline stage if it doesn't exist
+        var pipe_id = "m.grl/scheduler"
+        if (please.pipeline.__callbacks[pipe_id] === undefined) {
+            please.pipeline.add(-1, pipe_id, please.time.__schedule_handler);
         }
-        else {
-            batch.__pending.push(callback);
-            batch.__times.push(when);
-            if (!dirty) {
-                dirty = true;
-                
-                // register a pipeline stage if it doesn't exist
-                if (please.pipeline.__callbacks[pipe_id] === undefined) {
-                    please.pipeline.add(-1, pipe_id, frame_handler);
-                }
+    }
+};
+
+
+// [+] please.time.remove(callback)
+//
+// Removes a pending callback from the scheduler.
+//
+// - **callback** A function that was already scheduled 
+//   please.time.schedule.
+//
+please.time.remove = function (callback) {
+    var i = please.time.__pending.indexOf(callback);
+    if (i > -1) {
+        please.time.__pending.splice(i, 1);
+        please.time.__times.splice(i, 1);
+    }
+};
+
+
+// This hooks into the event loop and determines when pending events
+// should be called.
+please.time.__schedule_handler = function () {
+    if (please.time.__pending.length > 0) {
+        var stamp = performance.now();
+        please.time.__last_frame = stamp;
+
+        var pending = please.time.__pending;
+        var times = please.time.__times;
+        please.time.__pending = [];
+        please.time.__times = [];
+        var updates = 0;
+        ITER(i, pending) {
+            var callback = pending[i];
+            var when = times[i];
+            if (when <= stamp) {
+                updates += 1;                
+                callback(stamp);
             }
-        }
-    };
-
-
-    // [+] please.time.remove(callback)
-    //
-    // Removes a pending callback from the scheduler.
-    //
-    // - **callback** A function that was already scheduled 
-    //   please.time.schedule.
-    //
-    batch.remove = function (callback) {
-        var i = batch.__pending.indexOf(callback);
-        if (i > -1) {
-            batch.__pending.splice(i, 1);
-            batch.__times.splice(i, 1);
-        }
-    };
-
-
-    var frame_handler= function () {
-        if (batch.__pending.length > 0) {
-            var stamp = performance.now();
-            batch.now = stamp;
-
-            var pending = batch.__pending;
-            var times = batch.__times;
-            batch.__pending = [];
-            batch.__times = [];
-            var updates = 0;
-            ITER(i, pending) {
-                var callback = pending[i];
-                var when = times[i];
-                if (when <= stamp) {
-                    updates += 1;                
-                    callback(stamp);
-                }
-                else {
-                    batch.__pending.push(callback);
-                    batch.__times.push(when);
-                }
-            };
-        }
-    };
-
-
-    return batch;
-})();
+            else {
+                please.time.__pending.push(callback);
+                please.time.__times.push(when);
+            }
+        };
+    }
+};
 
 
 // [+] please.path_driver(path, period, repeat, oscilate)
