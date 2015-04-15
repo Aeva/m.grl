2ec5964b5e93734793a7c2aa1bdfb1162e90c2ab
Aeva Palecek <aeva@lunarfracking.com>
2015-04-14 22:00:03 -0500

    merged regen.sh into the main build script

--- build.sh
+++ build.sh
@@ -1,1 +1,12 @@
+#!/usr/bin/env sh
+
+# build m.grl
 sh jspp.sh src/m.header.js mgrl.js -DBSIDES -DWEBGL
+
+# copy core lib into the project templates
+cp mgrl.js templates/basic_project/libs/
+
+# regenerate template archives
+cd templates
+rm *.zip
+zip -r basic_project.zip basic_project
