07eebf4448633d01f82bd4644690cf4758fd67ec
Aeva Palecek <aeva@lunarfracking.com>
2015-07-12 01:49:35 -0500

    #37 building out more particle functionality

--- mgrl.js
+++ mgrl.js
@@ -6704,39 +6704,49 @@ please.StructArray = function (struct, count) {
 please.StructView = function (struct_array) {
     console.assert(this !== window);
     this.__array = struct_array;
-    this.__index = 0;
-    this.cache = {};
-    var add_handle = function (name, type, offset) {
+    this.__index = null;
+    this.__cache = {};
+    var add_handle = function (name, type, cache) {
         Object.defineProperty(this, name, {
             get : function () {
-                return this.cache[name];
+                return cache[name];
             },
             set : function (val) {
                 for (var i=0; i<type; i+=1) {
-                    this.cache[name][i] = val[i];
+                    cache[name][i] = (typeof(val) === "number") ? val : val[i];
                 }
-                return this.cache[name];
+                return cache[name];
             },
         });
     }.bind(this);
-    var pointer = 0;
     for (var i=0; i<this.__array.struct.length; i+=1) {
         var name = this.__array.struct[i][0];
         var type = this.__array.struct[i][1];
-        add_handle(name, type, pointer);
-        pointer += type;
+        add_handle(name, type, this.__cache);
     }
-    this.focus(this.__index);
+    this.focus(0);
 };
 please.StructView.prototype = {
     "focus" : function (index) {
-        this.__index = index;
-        var pointer = this.__array.struct.size * index;
-        for (var i=0; i<this.__array.struct.length; i+=1) {
-            var name = this.__array.struct[i][0];
-            var type = this.__array.struct[i][1];
-            this.cache[name] = this.__array.blob.subarray(pointer, pointer+type);
-            pointer += type;
+        if (index !== this.__index) {
+            this.__index = index;
+            var ptr = this.__array.struct.size * index;
+            for (var i=0; i<this.__array.struct.length; i+=1) {
+                var name = this.__array.struct[i][0];
+                var type = this.__array.struct[i][1];
+                this.__cache[name] = this.__array.blob.subarray(ptr, ptr+type);
+                ptr += type;
+            }
+        }
+    },
+    "dub" : function (src, dest) {
+        var struct_size = this.__array.struct.size;
+        var index_a = struct_size * src;
+        var index_b = struct_size * dest;
+        var prt_a = this.__array.blob.subarray(index_a, index_a + struct_size);
+        var prt_b = this.__array.blob.subarray(index_b, index_b + struct_size);
+        for (var i=0; i<this.__array.struct.size; i+=1) {
+            prt_b[i] = prt_a[i];
         }
     },
 };
@@ -6780,23 +6790,24 @@ please.ParticleEmitter = function (asset, span, limit, setup, update, ext) {
     please.GraphNode.call(this);
     this.__is_particle_tracker = true;
     var tracker = this.__tracker = {};
-    if (asset.instance) {
+    if (typeof(asset.instance) === "function") {
         tracker.asset = asset;
         tracker.stamp = asset.instance();
+        tracker.stamp.use_manual_cache_invalidation();
         tracker.animated = !!tracker.stamp.play;
     }
     else {
         throw("Invalid asset.  Did you pass a GraphNode by mistake?");
     }
-    console.assert(span > 0 || typeof(span) === "function");
-    console.assert(limit > 0);
+    console.assert(typeof(span) === "number" || typeof(span) === "function");
+    console.assert(typeof(limit) === "number");
     console.assert(typeof(setup) === "function");
     console.assert(typeof(update) === "function");
     tracker.span = span;
     tracker.limit = limit;
     tracker.setup = setup;
     tracker.update = update;
-    var struct_def = [
+    var struct = [
         ["start", 1],
         ["expire", 1],
         ["world_matrix", 16],
@@ -6805,10 +6816,10 @@ please.ParticleEmitter = function (asset, span, limit, setup, update, ext) {
         for (var name in ext) if (ext.hasOwnProperty(name)) {
             var prop = ext[name];
             if (typeof(prop) === "number") {
-                struct_def.push([name, 1]);
+                struct.push([name, 1]);
             }
             else if (prop.length) {
-                struct_def.push([name, prop.length]);
+                struct.push([name, prop.length]);
             }
         }
         tracker.defaults = ext;
@@ -6816,16 +6827,21 @@ please.ParticleEmitter = function (asset, span, limit, setup, update, ext) {
     else {
         tracker.defaults = null;
     }
-    tracker.blob = new please.StructArray(struct_def, tracker.limit);
+    tracker.blob = new please.StructArray(struct, tracker.limit);
     tracker.view = new please.StructView(tracker.blob);
     tracker.live = 0;
 };
 please.ParticleEmitter.prototype = Object.create(please.GraphNode.prototype);
 // Add a new particle to the system
 please.ParticleEmitter.prototype.rain = function () {
+    if (tracker.live === tracker.limit) {
+        console.error("Cannot add any more particles to emitter.");
+        return;
+    }
     var tracker = this.__tracker;
+    var p_index = tracker.live;
     var particle = tracker.view;
-    var p_index = tracker.live % tracker.limit;
+    particle.focus(p_index);
     // upload defaults if applicable
     if (tracker.defaults) {
         for (var name in tracker.defaults) if (tracker.defaults.hasOwnProperty(name)) {
@@ -6840,10 +6856,7 @@ please.ParticleEmitter.prototype.rain = function () {
     }
     // initialize builtins
     var now = window.performance.now();
-    var span = tracker.span;
-    if (typeof(tracker.span) === "function") {
-        span = tracker.span();
-    }
+    var span = (typeof(tracker.span) === "function" ? tracker.span() : tracker.span);
     particle["start"][0] = now;
     particle["expire"][0] = now + span;
     mat4.copy(particle["world_matrix"], this.world_matrix);
@@ -6860,6 +6873,48 @@ please.ParticleEmitter.prototype.copy = function () {
 // Clear out all active particles from this system
 please.ParticleEmitter.prototype.clear = function () {
     this.__tracker.live = 0;
+};
+// Update and draw the particle system
+please.ParticleEmitter.prototype.draw = function() {
+    var tracker = this.__tracker.live;
+    var particle = tracker.view;
+    var age, now = please.pipeline.__framestart;
+    var cache = tracker.stamp.__ani_cache;
+    var first = true;
+    for (var i=0; i<tracker.live; i+=1) {
+        particle.focus(i);
+        if (now < particle["expire"]) {
+            // The particle is alive, so we will figure out its
+            // current age, and call the update function on it, and
+            // then draw the particle on screen.
+            age = now - particle["start"];
+            tracker.update.call(this, particle, age);
+            for (var k=0; k<tracker.blob.struct.length; k+=1) {
+                var name = tracker.blob.struct[i][0];
+                if (cache[name]) {
+                    cache[name] = particle[name];
+                }
+            }
+            // FIXME if the 'stamp' is animated, then we should adjust
+            // the animation frame accordingly before drawing.
+            tracker.stamp.__draw();
+        }
+        else {
+            // The particle is dead, so it should be removed.  This is
+            // done by writting it over with the information about the
+            // last particle in the blob, and decrementing the
+            // particle counter.  The 'i' index is also decremented so
+            // as a new particle is now in the same slot.
+            this.__on_die(i);
+            i -= 1;
+        }
+    }
+};
+// Called to remove a dead particle
+please.ParticleEmitter.prototype.__on_die = function(index) {
+    var tracker = this.__tracker;
+    tracker.view.dub(tracker.live-1, index);
+    tracker.live -= 1;
 };
 // - bundled glsl shader assets --------------------------------------------- //
 addEventListener("mgrl_gl_context_created", function () {
--- src/m.rain.js
+++ src/m.rain.js
@@ -40,17 +40,20 @@
 please.ParticleEmitter = function (asset, span, limit, setup, update, ext) {
     please.GraphNode.call(this);
     this.__is_particle_tracker = true;
+    
     var tracker = this.__tracker = {};
-    if (asset.instance) {
+    if (typeof(asset.instance) === "function") {
         tracker.asset = asset;
         tracker.stamp = asset.instance();
+        tracker.stamp.use_manual_cache_invalidation();
         tracker.animated = !!tracker.stamp.play;
     }
     else {
         throw("Invalid asset.  Did you pass a GraphNode by mistake?");
     }
-    console.assert(span > 0 || typeof(span) === "function");
-    console.assert(limit > 0);
+    
+    console.assert(typeof(span) === "number" || typeof(span) === "function");
+    console.assert(typeof(limit) === "number");
     console.assert(typeof(setup) === "function");
     console.assert(typeof(update) === "function");
     
@@ -59,7 +62,7 @@ please.ParticleEmitter = function (asset, span, limit, setup, update, ext) {
     tracker.setup = setup;
     tracker.update = update;
 
-    var struct_def = [
+    var struct = [
         ["start", 1],
         ["expire", 1],
         ["world_matrix", 16],
@@ -68,10 +71,10 @@ please.ParticleEmitter = function (asset, span, limit, setup, update, ext) {
         ITER_PROPS(name, ext) {
             var prop = ext[name];
             if (typeof(prop) === "number") {
-                struct_def.push([name, 1]);
+                struct.push([name, 1]);
             }
             else if (prop.length) {
-                struct_def.push([name, prop.length]);
+                struct.push([name, prop.length]);
             }
         }
         tracker.defaults = ext;
@@ -80,7 +83,7 @@ please.ParticleEmitter = function (asset, span, limit, setup, update, ext) {
         tracker.defaults = null;
     }
 
-    tracker.blob = new please.StructArray(struct_def, tracker.limit);
+    tracker.blob = new please.StructArray(struct, tracker.limit);
     tracker.view = new please.StructView(tracker.blob);
     tracker.live = 0;
 };
@@ -89,9 +92,14 @@ please.ParticleEmitter.prototype = Object.create(please.GraphNode.prototype);
 
 // Add a new particle to the system
 please.ParticleEmitter.prototype.rain = function () {
+    if (tracker.live === tracker.limit) {
+        console.error("Cannot add any more particles to emitter.");
+        return;
+    }
     var tracker = this.__tracker;
+    var p_index = tracker.live;
     var particle = tracker.view;
-    var p_index = tracker.live % tracker.limit;
+    particle.focus(p_index);
     
     // upload defaults if applicable
     if (tracker.defaults) {
@@ -108,10 +116,7 @@ please.ParticleEmitter.prototype.rain = function () {
 
     // initialize builtins
     var now = window.performance.now();
-    var span = tracker.span;
-    if (typeof(tracker.span) === "function") {
-        span = tracker.span();
-    }
+    var span = DYNAMIC(tracker.span);
     particle["start"][0] = now;
     particle["expire"][0] = now + span;
     mat4.copy(particle["world_matrix"], this.world_matrix);
@@ -133,4 +138,50 @@ please.ParticleEmitter.prototype.copy = function () {
 // Clear out all active particles from this system
 please.ParticleEmitter.prototype.clear = function () {
     this.__tracker.live = 0;
+};
+
+
+// Update and draw the particle system
+please.ParticleEmitter.prototype.draw = function() {
+    var tracker = this.__tracker.live;
+    var particle = tracker.view;
+    var age, now = please.pipeline.__framestart;
+    var cache = tracker.stamp.__ani_cache;
+    var first = true;
+    RANGE(i, tracker.live) {
+        particle.focus(i);
+        if (now < particle["expire"]) {
+            // The particle is alive, so we will figure out its
+            // current age, and call the update function on it, and
+            // then draw the particle on screen.
+            age = now - particle["start"];
+            tracker.update.call(this, particle, age);
+            ITER(k, tracker.blob.struct) {
+                var name = tracker.blob.struct[i][0];
+                if (cache[name]) {
+                    cache[name] = particle[name];
+                }
+            }
+            // FIXME if the 'stamp' is animated, then we should adjust
+            // the animation frame accordingly before drawing.
+            tracker.stamp.__draw();
+        }
+        else {
+            // The particle is dead, so it should be removed.  This is
+            // done by writting it over with the information about the
+            // last particle in the blob, and decrementing the
+            // particle counter.  The 'i' index is also decremented so
+            // as a new particle is now in the same slot.
+            this.__on_die(i);
+            i -= 1;
+        }
+    }
+};
+
+
+// Called to remove a dead particle
+please.ParticleEmitter.prototype.__on_die = function(index) {
+    var tracker = this.__tracker;
+    tracker.view.dub(tracker.live-1, index);
+    tracker.live -= 1;
 };
