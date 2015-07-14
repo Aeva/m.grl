7f583c3fb248919bc86d54fd05422eb567144a19
Aeva Palecek <aeva@lunarfracking.com>
2015-07-13 23:45:39 -0500

    #37 - particle system works now

--- mgrl.js
+++ mgrl.js
@@ -6765,39 +6765,12 @@ please.StructView.prototype = {
  * This file provides M.GRL's particle system.
  * 
  */
-// [+] please.ParticleEmitter(asset, config, setup, update)
-//
-// ```
-// // We pass an asset, not an asset instance to the particle tracker.
-// // The asset must have an instance method defined which returns a
-// // GraphNode.
-// var stamp = please.access("explosion.gani");
-// var particle_setup = function (data) {
-//   // set the initial particle data and return it
-//   return data;
-// };
-// var particle_update = function (data, delta_time) {
-//   // update the particle data and return it
-//   return data;
-// };
-// var struct = {
-//   "color" : [0, 0, 1, 1],
-// };
-// var emitter = please.particle_system(stamp, life_span, max_count, particle_setup, particle_update, struct);
-// graph.add(emitter);
-//
-// for (var i=0; i<10; i+=1) {
-//   // create a new particle within the particle system
-//   emitter.rain();
-// }
-//
-// // make another emitter with the same params
-//  var another = emitter.copy();
-// ```
+// [+] please.ParticleEmitter(asset, span, limit, setup, update, ext)
 //
 please.ParticleEmitter = function (asset, span, limit, setup, update, ext) {
     please.GraphNode.call(this);
     this.__is_particle_tracker = true;
+    this.__drawable = true;
     var tracker = this.__tracker = {};
     if (typeof(asset.instance) === "function") {
         tracker.asset = asset;
@@ -6834,8 +6807,12 @@ please.ParticleEmitter = function (asset, span, limit, setup, update, ext) {
         tracker.defaults = ext;
     }
     else {
-        tracker.defaults = null;
+        tracker.defaults = {};
     }
+    tracker.var_names = [];
+    for (var i=0; i<struct.length; i+=1) {
+        tracker.var_names.push(struct[i][0]);
+    };
     tracker.blob = new please.StructArray(struct, tracker.limit);
     tracker.view = new please.StructView(tracker.blob);
     tracker.last = window.performance.now();
@@ -6844,11 +6821,11 @@ please.ParticleEmitter = function (asset, span, limit, setup, update, ext) {
 please.ParticleEmitter.prototype = Object.create(please.GraphNode.prototype);
 // Add a new particle to the system
 please.ParticleEmitter.prototype.rain = function () {
+    var tracker = this.__tracker;
     if (tracker.live === tracker.limit) {
         console.error("Cannot add any more particles to emitter.");
         return;
     }
-    var tracker = this.__tracker;
     var p_index = tracker.live;
     var particle = tracker.view;
     particle.focus(p_index);
@@ -6869,7 +6846,7 @@ please.ParticleEmitter.prototype.rain = function () {
     var span = (typeof(tracker.span) === "function" ? tracker.span() : tracker.span);
     particle["start"][0] = now;
     particle["expire"][0] = now + span;
-    mat4.copy(particle["world_matrix"], this.world_matrix);
+    mat4.copy(particle["world_matrix"], this.shader.world_matrix);
     // call the particle initialization method
     tracker.setup.call(this, particle);
     tracker.live += 1;
@@ -6886,11 +6863,12 @@ please.ParticleEmitter.prototype.clear = function () {
 };
 // Update and draw the particle system
 please.ParticleEmitter.prototype.draw = function() {
-    var tracker = this.__tracker.live;
+    var tracker = this.__tracker;
     var particle = tracker.view;
     var cache = tracker.stamp.__ani_cache;
+    var store = tracker.stamp.__ani_store;
     var now = please.pipeline.__framestart;
-    var last = now - tracker.last;
+    var delta = now - tracker.last;
     tracker.last = now;
     // To quickly draw all of the particles with a single objcet, the
     // object is set to manual cache invalidation and we overwrite
@@ -6898,19 +6876,31 @@ please.ParticleEmitter.prototype.draw = function() {
     // us to override the world matrix driver.
     for (var i=0; i<tracker.live; i+=1) {
         particle.focus(i);
-        if (now < particle["expire"]) {
+        if (now < particle["expire"][0]) {
             // The particle is alive, so we will figure out its
             // current age, and call the update function on it, and
             // then draw the particle on screen.
-            tracker.update.call(this, particle);
-            for (var k=0; k<tracker.blob.struct.length; k+=1) {
-                var name = tracker.blob.struct[i][0];
-                if (cache[name]) {
-                    cache[name] = particle[name];
+            tracker.update.call(this, particle, delta);
+            for (var name in this.shader) if (this.shader.hasOwnProperty(name)) {
+                if (tracker.var_names.indexOf(name) !== -1) {
+                    if (store[name]) {
+                        store[name] = particle[name];
+                        cache[name] = null;
+                    }
+                }
+                else {
+                    var copy_from = this;
+                    if (this.__ani_store[name] === undefined || this.__ani_store[name] === null) {
+                        copy_from = tracker.stamp;
+                    }
+                    store[name] = copy_from.__ani_store[name];
+                    cache[name] = copy_from.__ani_cache[name];
                 }
             }
             // FIXME if the 'stamp' is animated, then we should adjust
-            // the animation frame accordingly before drawing.
+            // the animation frame accordingly before drawing.  This
+            // might be only really possible with ganis, but that is ok.
+            tracker.stamp.__bind();
             tracker.stamp.__draw();
         }
         else {
--- src/m.rain.js
+++ src/m.rain.js
@@ -7,39 +7,12 @@
  */
 
 
-// [+] please.ParticleEmitter(asset, config, setup, update)
-//
-// ```
-// // We pass an asset, not an asset instance to the particle tracker.
-// // The asset must have an instance method defined which returns a
-// // GraphNode.
-// var stamp = please.access("explosion.gani");
-// var particle_setup = function (data) {
-//   // set the initial particle data and return it
-//   return data;
-// };
-// var particle_update = function (data, delta_time) {
-//   // update the particle data and return it
-//   return data;
-// };
-// var struct = {
-//   "color" : [0, 0, 1, 1],
-// };
-// var emitter = please.particle_system(stamp, life_span, max_count, particle_setup, particle_update, struct);
-// graph.add(emitter);
-//
-// for (var i=0; i<10; i+=1) {
-//   // create a new particle within the particle system
-//   emitter.rain();
-// }
-//
-// // make another emitter with the same params
-//  var another = emitter.copy();
-// ```
+// [+] please.ParticleEmitter(asset, span, limit, setup, update, ext)
 //
 please.ParticleEmitter = function (asset, span, limit, setup, update, ext) {
     please.GraphNode.call(this);
     this.__is_particle_tracker = true;
+    this.__drawable = true;
     
     var tracker = this.__tracker = {};
     if (typeof(asset.instance) === "function") {
@@ -80,8 +53,13 @@ please.ParticleEmitter = function (asset, span, limit, setup, update, ext) {
         tracker.defaults = ext;
     }
     else {
-        tracker.defaults = null;
+        tracker.defaults = {};
     }
+
+    tracker.var_names = [];
+    ITER(i, struct) {
+        tracker.var_names.push(struct[i][0]);
+    };
 
     tracker.blob = new please.StructArray(struct, tracker.limit);
     tracker.view = new please.StructView(tracker.blob);
@@ -93,11 +71,11 @@ please.ParticleEmitter.prototype = Object.create(please.GraphNode.prototype);
 
 // Add a new particle to the system
 please.ParticleEmitter.prototype.rain = function () {
+    var tracker = this.__tracker;
     if (tracker.live === tracker.limit) {
         console.error("Cannot add any more particles to emitter.");
         return;
     }
-    var tracker = this.__tracker;
     var p_index = tracker.live;
     var particle = tracker.view;
     particle.focus(p_index);
@@ -120,7 +98,7 @@ please.ParticleEmitter.prototype.rain = function () {
     var span = DYNAMIC(tracker.span);
     particle["start"][0] = now;
     particle["expire"][0] = now + span;
-    mat4.copy(particle["world_matrix"], this.world_matrix);
+    mat4.copy(particle["world_matrix"], this.shader.world_matrix);
     
     // call the particle initialization method
     tracker.setup.call(this, particle);
@@ -144,11 +122,12 @@ please.ParticleEmitter.prototype.clear = function () {
 
 // Update and draw the particle system
 please.ParticleEmitter.prototype.draw = function() {
-    var tracker = this.__tracker.live;
+    var tracker = this.__tracker;
     var particle = tracker.view;
     var cache = tracker.stamp.__ani_cache;
+    var store = tracker.stamp.__ani_store;
     var now = please.pipeline.__framestart;
-    var last = now - tracker.last;
+    var delta = now - tracker.last;
     tracker.last = now;
     
     // To quickly draw all of the particles with a single objcet, the
@@ -158,20 +137,31 @@ please.ParticleEmitter.prototype.draw = function() {
     
     RANGE(i, tracker.live) {
         particle.focus(i);
-        if (now < particle["expire"]) {
+        if (now < particle["expire"][0]) {
             // The particle is alive, so we will figure out its
             // current age, and call the update function on it, and
             // then draw the particle on screen.
-            tracker.update.call(this, particle);
-            ITER(k, tracker.blob.struct) {
-                var name = tracker.blob.struct[i][0];
-                if (cache[name]) {
-                    cache[name] = particle[name];
+            tracker.update.call(this, particle, delta);
+            ITER_PROPS(name, this.shader) {
+                if (tracker.var_names.indexOf(name) !== -1) {
+                    if (store[name]) {
+                        store[name] = particle[name];
+                        cache[name] = null;
+                    }
                 }
-            }
+                else {
+                    var copy_from = this;
+                    if (this.__ani_store[name] === undefined || this.__ani_store[name] === null) {
+                        copy_from = tracker.stamp;
+                    }
+                    store[name] = copy_from.__ani_store[name];
+                    cache[name] = copy_from.__ani_cache[name];
+                }
+            }            
             // FIXME if the 'stamp' is animated, then we should adjust
             // the animation frame accordingly before drawing.  This
             // might be only really possible with ganis, but that is ok.
+            tracker.stamp.__bind();
             tracker.stamp.__draw();
         }
         else {
