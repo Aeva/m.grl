f02bb941cb09063d56b86a7fdcefa3bd2077fd3d
Aeva Palecek <aeva@lunarfracking.com>
2015-07-19 16:00:58 -0500

    #37 - building out snowy particle demo

---
+++ demos/particles/glsl/custom.frag
@@ -0,0 +1,84 @@
+
+precision mediump float;
+
+uniform sampler2D diffuse_texture;
+
+uniform float alpha;
+uniform bool is_sprite;
+uniform bool is_transparent;
+uniform float mgrl_frame_start;
+
+varying vec3 local_position;
+varying vec3 local_normal;
+varying vec2 local_tcoords;
+varying vec3 world_position;
+varying vec3 screen_position;
+
+uniform bool is_floor;
+
+const vec3 cone_a = vec3(4.87, 0.0, 11.626024);
+const vec3 cone_b = vec3(-4.87, 0.0, 11.626024);
+const float angle = 3.5;
+const vec2 falloff = vec2(14.0, 10.0);
+const vec2 horizon = vec2(20.0, 50.0);
+
+void main(void) {
+  vec4 diffuse_color;
+  
+  if (is_floor) {
+
+    // procedural texture for the ground
+    float checker_scale = 8.0;
+    bool check_x = fract((world_position.x+2.0) / checker_scale) < 0.5;
+    bool check_y = fract((world_position.y-2.0) / checker_scale) < 0.5;
+    if ((check_x && check_y) || (!check_x && !check_y)) {
+      diffuse_color = vec4(0.55, 0.55, 0.55, 1.0);
+    }
+    else {
+      diffuse_color = vec4(0.6, 0.6, 0.6, 1.0);
+    }
+  }
+  else {
+
+    // use textures for everything else
+    diffuse_color = texture2D(diffuse_texture, local_tcoords);
+    if (is_sprite) {
+      float cutoff = is_transparent ? 0.1 : 1.0;
+      if (diffuse_color.a < cutoff) {
+        discard;
+      }
+    }
+    diffuse_color.a *= alpha;
+  }
+
+
+  // lighting
+  float illuminated = 0.0;
+  float dist_a = distance(world_position, cone_a);
+  float dist_b = distance(world_position, cone_b);
+  vec3 cone = dist_a < dist_b ? cone_a : cone_b;
+  if (world_position.z < cone.z) {
+    float scale = distance(world_position.z, cone.z);
+    if (distance(world_position.xy, cone.xy) <= scale*angle) {
+      float dist = min(dist_a, dist_b);
+      illuminated = 1.0 - (clamp(dist - falloff.x, 0.0, falloff.y) / falloff.y);
+    }
+  }
+
+  
+  // apply lighting
+  vec3 dim_color = diffuse_color.rgb - 0.4;
+  vec3 bright_color = diffuse_color.rgb;
+  vec3 applied_light = mix(dim_color, bright_color, illuminated);
+  vec4 combined_color = vec4(applied_light, diffuse_color.a);
+
+
+  // determine the horizon falloff
+  float dist = distance(world_position.xy, vec2(0.0, 0.0));
+  if (dist > horizon.x) {
+    combined_color.a = 1.0 - (clamp(dist - horizon.x, 0.0, horizon.y) / horizon.y);
+  }
+
+  
+  gl_FragColor = combined_color;
+}
---
+++ demos/particles/project.js
@@ -0,0 +1,225 @@
+"use strict";
+/*
+
+ Midnight Graphics & Recreation Library Project Template
+
+ This javascript source file has been dedicated to the public domain
+ by way of CC0.  More information about CC0 is available here:
+ https://creativecommons.org/publicdomain/zero/1.0/ .
+
+ Art assets used are under a Creative Commons Attribution - Share
+ Alike license or similar (this is explained in detail elsewhere).
+
+ M.GRL itself is made available to you under the LGPL.
+
+ M.GRL makes use of the glMatrix library, which is some variety of BSD
+ license.
+
+ Have a nice day! ^_^
+
+*/
+
+
+// local namespace
+var demo = {
+    "viewport" : null, // the render pass that will be rendered
+    "loading" : {}, // used by loading screen
+    "main" : {}, // used for main demo
+};
+
+
+addEventListener("load", function() {
+    // Attach the opengl rendering context.  This must be done before
+    // anything else.
+    please.gl.set_context("gl_canvas", {
+        antialias : false,
+    });
+
+    // Set OpenGL rendering state defaults directly.  Some of this may
+    // be abstracted by m.grl in the future.
+    gl.enable(gl.DEPTH_TEST);
+    gl.depthFunc(gl.LEQUAL);
+    gl.enable(gl.CULL_FACE);
+
+    // Define where m.grl is to find various assets when using the
+    // load methed.
+    please.set_search_path("glsl", "glsl/");
+    please.set_search_path("gani", "../lpc_assets/keyframes/");
+    please.set_search_path("img", "../gl_assets/img/");
+    please.set_search_path("jta", "../gl_assets/models/");
+    
+    // Queue up assets to be downloaded before the game starts.
+    please.load("custom.frag");
+    please.load("snow_flakes.png");
+    //please.load("coin.gani");
+    please.load("lamp_post.jta");
+
+    // Register a render passes with the scheduler.  The autoscale
+    // prefab is used to change the dimensions of the rendering canvas
+    // when it has the 'fullscreen' css class, as well as constrain
+    // the maximum height of said canvas element.  You are responsible
+    // for providing the css needed to upsample the canvas, though
+    // this project template accomplishes that for you.  See "ui.css".
+    please.pipeline.add_autoscale();
+
+    // register a render pass with the scheduler
+    please.pipeline.add(10, "project/draw", function () {
+        please.render(demo.viewport);
+    }).skip_when(function () { return demo.viewport === null; });
+
+    // start the rendering pipeline
+    please.pipeline.start();
+
+    // Show a loading screen
+    setup_loading_screen();
+});
+
+
+var setup_loading_screen = function () {
+    // This function sets up a loading screen.
+    
+    demo.loading.renderer = new please.LoadingScreen();
+    demo.viewport = demo.loading.renderer;
+
+    (function percent () {
+        if (please.media.pending.length > 0) {
+            var progress = please.media.get_progress();
+            if (progress.all > -1) {
+                var label = document.getElementById("loading_screen");
+                label.innerHTML = "" + Math.round(progress.all) + "%";
+            }
+            setTimeout(percent, 100);
+        }
+    })();
+};
+
+
+addEventListener("mgrl_fps", function (event) {
+    // This handler is called every so often to report an estimation
+    // of the current frame rate, so that it can be displayed to the
+    // user.
+    document.getElementById("fps").innerHTML = event.detail;
+});
+
+
+addEventListener("mgrl_media_ready", please.once(function () {
+    // Build the default shader
+    var prog = please.glsl("default", "simple.vert", "custom.frag");
+    prog.activate();
+        
+    // Scere Graph object
+    var graph = demo.main.graph = new please.SceneGraph();
+
+    // Add a camera
+    var camera = demo.main.camera = new please.CameraNode();
+    // camera.look_at = [5, 0, 8];
+    // camera.location = [-13.47, -18.97, .9];
+    camera.look_at = [5, 0, 7];
+    camera.location = [-15, -20, 4.0];
+
+    graph.add(camera);
+    camera.activate();
+
+    // Add a floor
+    var floor = new FloorNode();
+    floor.use_manual_cache_invalidation();
+    graph.add(floor);
+
+    // Add a fixture in the middle of the floor
+    var lamp = please.access("lamp_post.jta").instance();
+    graph.add(lamp);
+    demo.main.lamp = lamp;
+    lamp.shader.is_floor = false;
+    lamp.use_manual_cache_invalidation();
+
+    // Add some particle effect thing
+    var fountain = new ParticleFountain();
+    fountain.scale = [1.5, 1.5, 1.5];
+    graph.add(fountain);
+    demo.main.fountain = fountain;
+    for (var i=0; i<800; i+=1) {
+        fountain.rain();
+    }
+    
+    // Add a renderer using the default shader.
+    var renderer = demo.main.renderer = new please.RenderNode("default");
+    renderer.clear_color = [.15, .15, .15, 1];
+    renderer.graph = graph;
+    
+    // Add a timeout before the screen wipe to allow images etc to
+    // upload to the gpu, otherwise the transition will be choppy.
+    window.setTimeout(function () {
+        // Hide the loading screen html overlay.
+        document.getElementById("loading_screen").style.display = "none";
+
+        // An effect node is used to blend between the two render
+        // nodes.  In this case, we're using the disintigration
+        // effect.
+        var fade_out = new please.Disintegrate();
+        fade_out.blend_between(
+            demo.loading.renderer,
+            demo.main.renderer,
+            1500);
+        fade_out.shader.px_size = 50;
+        demo.viewport = fade_out;
+    }, 2000);
+}));
+
+
+var ParticleFountain = function() {
+    //var asset = please.access("coin.gani");
+    var asset = please.access("snow_flakes.png");
+    var span = Infinity;
+    var limit = 1000;
+    var ext = {
+        "vector" : [0,0,0],
+        "skitter" : 1,
+        "alpha" : 1,
+    };
+
+    var area = 15;
+
+    var setup = function (particle) {
+        var coord = [
+            (Math.random()*area)-(area*.5),
+            (Math.random()*area)-(area*.5),
+            Math.random()*15+1];
+        mat4.translate(
+            particle.world_matrix, particle.world_matrix, coord);
+
+        particle.vector = [
+            (Math.random()*1)-.5,
+            (Math.random()*1)-.5,
+            0];
+
+        particle.skitter = Math.random()+0.5;
+        particle.alpha = Math.random()*.8;
+    };
+    var update = function (particle, dt) {
+        var angle = please.degrees(dt/100000) * particle.skitter[0];
+        mat4.rotateZ(particle.world_matrix, particle.world_matrix, angle);
+        mat4.translate(
+            particle.world_matrix, particle.world_matrix, particle.vector);
+    };
+    var emitter = new please.ParticleEmitter(asset, span, limit, setup, update, ext);
+    emitter.shader.is_floor = false;
+    return emitter;
+};
+
+
+var FloorNode = function () {
+    console.assert(this !== window);
+    please.GraphNode.call(this);
+
+    this.__vbo = please.gl.make_quad(500, 500);
+    this.__drawable = true;
+    this.shader.is_floor = true;
+
+    this.bind = function () {
+        this.__vbo.bind();
+    };
+    this.draw = function () {
+        this.__vbo.draw();
+    };
+};
+FloorNode.prototype = please.GraphNode.prototype;
