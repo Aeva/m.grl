636f5e64d3cb973b06e46ea866b215fdf53f09c2
Aeva Palecek <aeva@lunarfracking.com>
2015-09-16 23:38:01 -0500

    #149 - tokenizer recognizes 'punctuation' symbols now

--- src/m.glsl_ast.js
+++ src/m.glsl_ast.js
@@ -18,6 +18,11 @@ please.gl.ast.Comment = function (text, multiline) {
 };
 
 
+please.gl.ast.Block = function () {
+};
+
+
+
 please.gl.__find_comments = function (src) {
     var open_regex = /(?:\/\/|\/\*)/m;
     var open = open_regex.exec(src);
@@ -52,10 +57,59 @@ please.gl.__find_comments = function (src) {
 };
 
 
+please.gl.__symbols = [
+    "(", ")", "{", "}", "[", "]",
+    "+=", "-=", "*=", "/=",
+    "+", "-", "*", "/",
+    "==", "&&", "<=", ">=", "<<", ">>", "||",
+    "<", ">", "=", "&",
+    ",", ";",
+];
+
+
+please.gl.__split_tokens = function (text) {
+    var lowest_symbol = null;
+    var lowest_offset = Infinity;
+
+    ITER(i, please.gl.__symbols) {
+        var symbol = please.gl.__symbols[i];
+        var offset = text.indexOf(symbol);
+        if (offset !== -1 && offset < lowest_offset) {
+            lowest_symbol = symbol;
+            lowest_offset = offset;
+        }
+    }
+
+    var tokens = [];
+    if (lowest_symbol) {
+        if (lowest_offset > 0) {
+            tokens.push(text.slice(0, lowest_offset).trim());
+        }
+        tokens.push(lowest_symbol);
+        var after = text.slice(lowest_offset + lowest_symbol.length).trim();
+        if (after.length > 0) {
+            tokens = tokens.concat(please.gl.__split_tokens(after));
+        }
+    }
+    else {
+        tokens = [text];
+    }
+    
+    return tokens;
+};
+
+
 please.gl.tokenize_glsl = function (src) {
     src = src.replace("\r", "\n");
-    var tokens = please.gl.__find_comments_single(src);
-
-
+    var tokens = [];
+    var tmp = please.gl.__find_comments(src);
+    ITER(i, tmp) {
+        if (tmp[i].constructor === String) {
+            tokens = tokens.concat(please.gl.__split_tokens(tmp[i]));
+        }
+        else {
+            tokens.push(tmp[i]);
+        }
+    }
     return tokens;
 };
