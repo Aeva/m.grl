ed66a21e56eaeb69128be7b209bdf6eea01b2ee8
Aeva Palecek <aeva@lunarfracking.com>
2016-01-20 22:08:16 -0600

    regenerated files

--- mgrl.js
+++ mgrl.js
@@ -2523,7 +2523,7 @@ please.pipeline.__on_draw_gl = function () {
     }
     var prog = please.gl.__cache.current;
     if (prog) {
-        prog.vars.mgrl_frame_start = start_time;
+        prog.vars.mgrl_frame_start = start_time/1000.0;
     }
     // render the pipeline stages
     var stage, msg = null, reset_name_bool = false;
