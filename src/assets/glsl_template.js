999b29982e731ca51bf53a6797286476df91c9c0
Aeva Palecek <aeva@lunarfracking.com>
2016-03-24 22:11:22 -0500

    bugfix for some versions of webkit

--- src/assets/glsl_template.js
+++ src/assets/glsl_template.js
@@ -6,7 +6,7 @@
     please.prop_map(please.__bundled_glsl, function (name, src) {
         addEventListener("mgrl_gl_context_created", function () {
             // see m.media.js's please.media.handlers.glsl for reference:
-            please.media.assets[name] = new please.gl.ShaderSource(atob(src), name);
+            please.media.assets[name] = new please.gl.ShaderSource(atob(src.replace(/\s/g, '')), name);
             please.media.assets[name].bundled = true;
         });
     });
