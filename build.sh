887ff7eddfafb4fef59e295cb4573c1fab268e20
Aeva Palecek <aeva@lunarfracking.com>
2016-07-02 01:31:19 -0500

    removed the rest of the 'tmp' dir stuff from build script

--- build.sh
+++ build.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env sh
 
-rm -rf tmp
-cp -r src tmp
-
 # bundle assets
 python regen_asset_bundle.py
 
 # build m.grl
-sh jspp.sh tmp/m.header.js mgrl.js -DBSIDES -DDOM -DWEBGL -DGLSL_ASSETS -DASSETS
+sh jspp.sh src/m.header.js mgrl.js -DBSIDES -DDOM -DWEBGL -DGLSL_ASSETS -DASSETS
 
 # apply extra transforms
 python reflow.py mgrl.js
@@ -21,5 +18,4 @@ cd templates
 cp common_assets/libs/* basic_project/libs/
 
 rm *.zip
-rm -rf tmp
 zip -r basic_project.zip basic_project
