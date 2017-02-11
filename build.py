72ec4a437de6f829b10154ba6481c97e07bf91d7
Aeva Palecek <aeva@lunarfracking.com>
2017-02-11 16:50:48 -0600

    New python-based build script build.py.
    This is part of issue #249.

---
+++ build.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import os
+import argparse
+import subprocess
+
+
+def run(cmd):
+    subprocess.call(cmd.split(" "))
+
+
+def parse_args():
+    options = {
+        "debug" : "Build with extra debugging assertions.",
+        "no-gl" : "Build without WebGL rendering support.",
+        "no-dom" : "Build without 2D DOM rendering support.",
+    }
+    args = argparse.ArgumentParser(description='M.GRL build tool.')
+    for key, value in options.items():
+        args.add_argument("--"+key, help=value, action='store_true')
+    return args.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    flags = {
+        "all" : ['bsides', 'assets'],
+        "gl" : ['webgl', 'glsl_assets'],
+        "dom" : ['dom'],
+        "debug" : ['debug'],
+    }
+    build_flags = []
+    def format_flags(group, active=True):
+        suffix = '' if active else '=0'
+        new_flags = ["-D" + flag.upper() + suffix for flag in flags[group]]
+        return new_flags
+
+    build_flags += format_flags("all")
+    build_flags += format_flags("gl", not args.no_gl)
+    build_flags += format_flags("dom", not args.no_dom)
+    build_flags += format_flags("debug", args.debug)
+    build_flags = ' '.join(build_flags)
+
+    build_cmd = "sh jspp.sh src/m.header.js mgrl.js " + build_flags
+
+    # generate asset bundle
+    run("python regen_asset_bundle.py")
+
+    # compile M.GRL
+    run(build_cmd)
+
+    # m.grl post processor
+    run("python reflow.py mgrl.js")
+
+    # regenerate the project templates archive
+    run("python reflow.py m.grl")
+    run("cp mgrl.js templates/common_assets/libs/")
+    run("cp theme/* templates/common_assets/libs/")
+    os.chdir("templates")
+    run("cp common_assets/libs/* basic_project/libs/")
+    run("rm *.zip")
+    run("zip -r basic_project.zip basic_project")
+
+    print "\n <3 M.GRL has been built with the following flags:\n" + build_flags
